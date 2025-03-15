b7529ecb958c7b500122bd4461dee6ed1956c8d9
ParveenKhan <erparveenkhan@gmail.com>
2025-03-15 15:22:02 +0530

    Initial Commit

---
+++ MedBook/MedBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedBookApp.swift
+//  MedBook
+//
+//  Created by ParveenKhan on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct MedBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MedBookTests/MedBookTests.swift
@@ -0,0 +1,17 @@
+//
+//  MedBookTests.swift
+//  MedBookTests
+//
+//  Created by ParveenKhan on 15/03/25.
+//
+
+import Testing
+@testable import MedBook
+
+struct MedBookTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
